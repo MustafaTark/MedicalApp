45570780fcac196ba659b6eb64f19d0f17ffb62f
mostafatarek <mostaf01230@gmail.com>
2023-04-13 17:43:08 +0200

    update Product Response

--- MedicalApp_BusinessLayer/Dto/ProductDto.cs
+++ MedicalApp_BusinessLayer/Dto/ProductDto.cs
@@ -8,7 +8,7 @@ namespace MedicalApp_BusinessLayer.Dto
 {
     public class ProductDto
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public double? Price { get; set; }
