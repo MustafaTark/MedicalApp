359aced4e663626bcc0f29b09d7fb5c3903843eb
mostafatarek <mostaf01230@gmail.com>
2023-04-14 17:52:51 +0200

    edit pharmacy response

--- MedicalApp_BusinessLayer/Dto/PharmacyDto.cs
+++ MedicalApp_BusinessLayer/Dto/PharmacyDto.cs
@@ -9,6 +9,7 @@ namespace MedicalApp_BusinessLayer.Dto
     public class PharmacyDto
     {
         public string? Id { get; set; }
+        public string? UserName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
--- MedicalApp_BusinessLayer/Dto/PharmacyForUpdateDto.cs
+++ MedicalApp_BusinessLayer/Dto/PharmacyForUpdateDto.cs
@@ -11,8 +11,6 @@ namespace MedicalApp_BusinessLayer.Dto
     {
        
         public string? UserName { get; set; }
-      
-        public string? Password { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Name { get; set; }
