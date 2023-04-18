d0e4b4c9a6b4d0b8a9df23f2c7e03eb7bf26fe21
Youssef Yousry <73970436+YoussefYousry@users.noreply.github.com>
2023-04-18 14:24:19 +0200

    Update Order Repo

--- MedicalApp/Controllers/PharmacyController.cs
+++ MedicalApp/Controllers/PharmacyController.cs
@@ -368,8 +368,8 @@ namespace MedicalApp.Controllers
                 return NotFound($"Patient with id : {patientId} doesn't exist in database");
             }
             var orders = await _repository.Order.GetOrdersForPatientAsync(patientId);
-            var ordersDto = _mapper.Map<IEnumerable<OrderDto>>(orders);
-            return Ok(ordersDto);
+            //var ordersDto = _mapper.Map<IEnumerable<OrderDto>>(orders);
+            return Ok(orders);
 
         }
         [HttpGet("OrderForPharmacy")]
